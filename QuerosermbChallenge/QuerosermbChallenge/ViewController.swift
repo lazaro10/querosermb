1a94b572ccc7fa0b4ea72e14e6d745edd5899bb6
Lázaro Santos <lazaro.lima.12@gmail.com>
2020-07-13 02:36:41 -0300

    add .gitignore

---
+++ QuerosermbChallenge/QuerosermbChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuerosermbChallenge
+//
+//  Created by Lazaro on 13/07/20.
+//  Copyright © 2020 LimasCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
