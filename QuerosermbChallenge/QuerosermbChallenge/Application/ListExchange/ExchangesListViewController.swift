e2ed8ef1a09e95ee3266278b72f4268acfd9dbec
Lázaro Santos <lazaro.lima.12@gmail.com>
2020-07-20 02:15:27 -0300

    Add files

--- QuerosermbChallenge/QuerosermbChallenge/Application/ListExchange/ExchangesListViewController.swift
+++ QuerosermbChallenge/QuerosermbChallenge/Application/ListExchange/ExchangesListViewController.swift
@@ -7,28 +7,38 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
 final class ExchangesListViewController: UIViewController {
     
-    private let tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return tableView
-    }()
+    private let tableView  = UITableView()
+    private let snakeBarErrorView = SnakeBarErrorView()
+    private let loadingView = NVActivityIndicatorView(frame: .zero)
     
     private lazy var dataSource: ExchangesListDataSource = {
-        let dataSource = ExchangesListDataSource(tableView: tableView)
+        let completion: ExchangesListDataSource.CompletionHandler = { [weak self] in
+            self?.didSelect(adapter: $0)
+        }
+        
+        let completionRefresh: ExchangesListDataSource.CompletionHandlerRefresh = { [weak self] in
+            self?.loadExchanges()
+        }
+        
+        let dataSource = ExchangesListDataSource(tableView: tableView,
+                                                 completion: completion,
+                                                 completionRefresh: completionRefresh)
     
         return dataSource
     }()
     
-    private let viewModel: CurrenciesListViewModel
+    private let viewModel: ExchangesLisViewModel
+    private let delegate: ExchangesListViewDelegate
     
     // MARK: Initializer
     
-    init(viewModel: CurrenciesListViewModel) {
+    init(viewModel: ExchangesLisViewModel, delegate: ExchangesListViewDelegate) {
         self.viewModel = viewModel
+        self.delegate = delegate
         
         super.init(nibName: nil, bundle: nil)
     }
@@ -50,35 +60,59 @@ final class ExchangesListViewController: UIViewController {
         super.viewDidLoad()
         
         bindViewModel()
-        exchanges()
+        loadExchanges()
     }
     
     // MARK: Private functions
     
     private func setupView() {
-        view.backgroundColor = .black
+        view.accessibilityLabel = R.string.accessibilityLabels.exchangeList()
     }
-
+    
     private func setupLayout() {
-        view.addSubview(tableView)
-        
-        NSLayoutConstraint.activate([
+        view.addSubview(tableView, constraints: [
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+        
+        view.addSubview(snakeBarErrorView)
+        
+        view.addSubview(loadingView, constraints: [
+            loadingView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loadingView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loadingView.widthAnchor.constraint(equalToConstant: 50),
+            loadingView.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 
     private func bindViewModel() {
-        viewModel.exchangesViewModel.bind { (exchanges) in
-            DispatchQueue.main.async { [weak self] in
-                self?.dataSource.viewModels = exchanges
+        viewModel.exchangeItemAdapters.bind({ [weak self] exchanges in
+            DispatchQueue.main.async {
+                self?.dataSource.adapters = exchanges
             }
-        }
+        })
+        
+        viewModel.isLoading.bind({ [weak self] (isOn) in
+            DispatchQueue.main.async {
+                let operation = isOn ? NVActivityIndicatorView.startAnimating : NVActivityIndicatorView.stopAnimating
+                operation(self!.loadingView)()
+            }
+        })
+        
+        viewModel.onError.bind({ [weak self] (message) in
+            DispatchQueue.main.async {
+                self?.snakeBarErrorView.show(message)
+            }
+        })
     }
     
-    private func exchanges() {
-        viewModel.fetchExchanges()
+    private func loadExchanges() {
+        viewModel.loadExchanges()
+    }
+    
+    private func didSelect(adapter: ExchangeItemAdapter) {
+        delegate.didSelect(adapter: adapter)
     }
 }
